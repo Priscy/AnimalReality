88ca2ac1952ac5798b7d6ce3dfe2da98ebf042ed
Priscila <priscila.sierra.10@gmail.com>
2017-05-04 14:59:59 -0500

    VR scene working

--- Assets/App/Scripts/VirtualButtonManager.cs
+++ Assets/App/Scripts/VirtualButtonManager.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using Vuforia;
 
 public class VirtualButtonManager : MonoBehaviour, IVirtualButtonEventHandler{
     public GameObject model;
     public GameObject video;
+    public enum TriggerType
+    {
+        VR_TRIGGER,
+        AR_TRIGGER
+    }
+    
     private VideoPlayerHelper mvideoPlayerHelper;
-   
+    private TransitionManager mTransitionManager;
+    public TriggerType triggerType = TriggerType.AR_TRIGGER;
 	// Use this for initialization
 	void Start () {
         VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
@@ -14,8 +22,8 @@ public class VirtualButtonManager : MonoBehaviour, IVirtualButtonEventHandler{
         {
             vbs[i].RegisterEventHandler(this);
         }
-         
-	
+
+        mTransitionManager = FindObjectOfType<TransitionManager>();
 	}
 
     void OnEnable() {
@@ -27,6 +35,7 @@ public class VirtualButtonManager : MonoBehaviour, IVirtualButtonEventHandler{
 	void Update () {
 	
 	}
+   
 
     public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
 
@@ -60,13 +69,38 @@ public class VirtualButtonManager : MonoBehaviour, IVirtualButtonEventHandler{
                     videopb.VideoPlayer.Pause();
                 }
                 model.gameObject.SetActive(false);
+                
                 break;
+
+            case "btnVRElephant":
+                Debug.Log("Boton vr elephant pressed");
+                video.gameObject.SetActive(false);
+                if (videopb != null && videopb.CurrentState == VideoPlayerHelper.MediaState.PLAYING)
+                {
+                    videopb.VideoPlayer.Pause();
+                }
+                bool goingBackToVR = (triggerType == TriggerType.VR_TRIGGER);
+                mTransitionManager.Play(goingBackToVR);
+                StartCoroutine(ResetAfter(0.3f * mTransitionManager.transitionDuration));
+                break;
+
         }
 
   }
-    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
-        Debug.Log("Button released");
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Pressed");
     }
 
+    private IEnumerator ResetAfter(float seconds)
+    {
+
+        yield return new WaitForSeconds(seconds);
+
+        
+    }
+   
+
+    
 
 }
